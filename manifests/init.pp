c1b031d31c9787b9673670fbe141b6ed926ac6b4
Chris Ewing <caewing@fs.fed.us>
2018-12-22 14:52:42 -0800

    Added template and params for WebAgent.conf

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,6 +11,18 @@ class ca_sso_web_agent (
   String $properties_file,
   String $temp_location,
   String $version,
+
+  # Variables for WebAgent.conf template
+  String $agent_config_object,
+  String $agent_id_file,
+  Boolean $enable_local_config,
+  Optional[String] $enable_web_agent,
+  String $host_config_file,
+  String $load_plugin,
+  String $local_config_file,
+  Optional[String] $locale,
+  String $server_path,
+  
 ) {
 
   $installed_version = $::facts['ca_sso_web_agent_version']
@@ -24,5 +36,12 @@ class ca_sso_web_agent (
     #notify { "VERSION MATCH!!! --> version ${version} = installed version ${installed_version}": }
     #notify { "VERSION MATCH!!!": }
   #}
+  file { "${server_path}/WebAgent.conf":
+    ensure  => file,
+#    mode    => $settings_file_mode,
+#    owner   => $settings_file_owner,                                                                                                   
+#    group   => $settings_file_group,
+    content => template('ca_sso_web_agent/WebAgent.conf.erb'),
+  }
 
 }
