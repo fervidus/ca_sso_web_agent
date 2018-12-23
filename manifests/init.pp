f5666ae04646b612094a3b0e4f22c9291a8894d3
Chris Ewing <caewing@fs.fed.us>
2018-12-22 16:47:39 -0800

    Added templates for LocalConfig.conf and WebAgentTrace.conf. Updated params to be based off of install_dir.

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,18 +14,40 @@ class ca_sso_web_agent (
 
   # Variables for WebAgent.conf template
   String $agent_config_object,
-  String $agent_id_file,
+  #String $agent_id_file,
   Boolean $enable_local_config,
-  Optional[String] $enable_web_agent,
-  String $host_config_file,
-  String $load_plugin,
-  String $local_config_file,
-  Optional[String] $locale,
+  Boolean $enable_web_agent,
+  #String $host_config_file,
+  #String $load_plugin,
+  #String $local_config_file,
+  String $locale,
   String $server_path,
+  #String $web_agent_config_file,
+  #
+  # Variables for LocalConfig.conf template
+  Boolean $enable_log_file,
+  Boolean $enable_trace_file,
+  #String $log_file,
+  String $log_file_size,
+  #String $trace_config_file,
+  #String $trace_file,
+  String $trace_file_size,
+  #
+  # Variables for WebAgentTrace.conf template
+  String $trace_log_components,
+  String $trace_log_data,
   
 ) {
 
-  $installed_version = $::facts['ca_sso_web_agent_version']
+  $host_config_file            = "${install_dir}/config/SmHost.conf"
+  $installed_version           = $::facts['ca_sso_web_agent_version']
+  $load_plugin                 = "${install_dir}/bin/libHttpPlugin.so"
+  $local_config_file           = "${install_dir}/config/LocalConfig.conf"
+  $log_file                    = "${install_dir}/log/WebAgent.log"
+  $trace_config_file           = "${install_dir}/config/WebAgentTrace.conf"
+  $trace_file                  = "${install_dir}/log/WebAgentTrace.log"
+  $web_agent_config_file       = "${install_dir}/config/WebAgent.conf"
+
 
   if $version != $installed_version {
     contain ca_sso_web_agent::preinstall
@@ -36,12 +58,30 @@ class ca_sso_web_agent (
     #notify { "VERSION MATCH!!! --> version ${version} = installed version ${installed_version}": }
     #notify { "VERSION MATCH!!!": }
   #}
-  file { "${server_path}/WebAgent.conf":
+
+  # WebAgent.conf
+  file { $web_agent_config_file:
     ensure  => file,
 #    mode    => $settings_file_mode,
 #    owner   => $settings_file_owner,                                                                                                   
 #    group   => $settings_file_group,
     content => template('ca_sso_web_agent/WebAgent.conf.erb'),
+  }
+  # LocalConfig.conf
+  file { $local_config_file:
+    ensure  => file,
+#    mode    => $settings_file_mode,
+#    owner   => $settings_file_owner,                                                                                                   
+#    group   => $settings_file_group,
+    content => template('ca_sso_web_agent/LocalConfig.conf.erb'),
+  }
+  # WebAgentTrace.conf
+  file { $trace_config_file:
+    ensure  => file,
+#    mode    => $settings_file_mode,
+#    owner   => $settings_file_owner,                                                                                                   
+#    group   => $settings_file_group,
+    content => template('ca_sso_web_agent/WebAgentTrace.conf.erb'),
   }
 
 }
