e553160f2413752b980ae079e4782f0e9ad98754
Chris Ewing <caewing@fs.fed.us>
2018-12-23 23:05:33 -0800

    Replace hardcoded refs to /app/CA/webagent, replaced by dynamic install_dir in custom facts.

--- lib/facter/ca_sso_web_agent_policy_servers.rb
+++ lib/facter/ca_sso_web_agent_policy_servers.rb
@@ -1,8 +1,10 @@
-if File.file? '/app/CA/webagent/config/SmHost.conf'
+install_dir = Facter.value(:ca_sso_web_agent_install_dir)
+
+if File.file? "#{install_dir}/config/SmHost.conf"
   Facter.add(:ca_sso_web_agent_policy_servers) do
     confine kernel: 'Linux'
     setcode do
-      policy_servers = Facter::Core::Execution.exec('grep -i policyserver= /app/CA/webagent/config/SmHost.conf')
+      policy_servers = Facter::Core::Execution.exec("grep -i policyserver= #{install_dir}/config/SmHost.conf")
       policy_servers.split("\n")
     end
   end
--- lib/facter/ca_sso_web_agent_version.rb
+++ lib/facter/ca_sso_web_agent_version.rb
@@ -1,11 +1,10 @@
-# @TODO Use which smreghost and strip bin/smreghost to get the "$install_dir" path???? 
-# /app/CA/webagent/bin/smreghost
-#
-if File.file? '/app/CA/webagent/install_config_info/ca-wa-version.info'
+install_dir = Facter.value(:ca_sso_web_agent_install_dir)
+
+if File.file? "#{install_dir}/install_config_info/ca-wa-version.info"
   Facter.add(:ca_sso_web_agent_version) do
     confine kernel: 'Linux'
     setcode do
-      Facter::Core::Execution.exec("grep -i fullversion /app/CA/webagent/install_config_info/ca-wa-version.info | awk -F = '{print $2}'")
+      Facter::Core::Execution.exec("grep -i fullversion #{install_dir}/install_config_info/ca-wa-version.info | awk -F = '{print $2}'")
     end
   end
 end
