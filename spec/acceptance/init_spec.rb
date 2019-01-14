8d5eaaec19258686694f012d3da1971b1a402fa8
Chris Ewing <caewing@fs.fed.us>
2019-01-14 15:05:46 -0800

    Updated package name in prereq_packages. Added additional test cases.

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -16,7 +16,7 @@ registration_username           = 'registrar'
 version                         = '12.52.109.2620'
 
 describe 'Declare ca_sso_web_agent class:', unless: UNSUPPORTED_PLATFORMS.include?(fact('osfamily')) do
-  context 'With required parameters' do
+  context 'With required parameters and defaults' do
     pp = <<-MANIFEST
       class { 'ca_sso_web_agent':
         agent_config_object             => '#{agent_config_object}',
@@ -44,6 +44,12 @@ describe 'Declare ca_sso_web_agent class:', unless: UNSUPPORTED_PLATFORMS.includ
   end
 
   context 'after applying ca_sso_web_agent class' do
+    prereq_packages = ['binutils', 'gcc', 'keyutils-libs.i686', 'libidn', 'libidn.i686', 'libstdc++.i686', 'libXext.i686', 'libXrender.i686', 'libXtst.i686', 'ncurses-libs.i686', 'unzip']
+    prereq_packages.each do |package|
+      describe package(package) do
+        it { should be_installed }
+      end
+    end
     describe file('/usr/local/bin/smreghost') do
       it { should exist }
       it { should be_symlink }
@@ -74,6 +80,14 @@ describe 'Declare ca_sso_web_agent class:', unless: UNSUPPORTED_PLATFORMS.includ
       it { should contain('components:  AgentFramework, HTTPAgent, WebAgent').before(/^data:/) }
       it { should contain('data: Date, Time, Pid, Tid, SrcFile, Function, TransactionID, IPAddr, IPPort, AgentName, Resource, User, Message').after(/^components:/) }
     end
+    describe file('/etc/sysconfig/httpd') do
+      it { should exist }
+      it { should contain("NETE_WA_ROOT=#{install_dir}").before(/^NETE_WA_PATH/) }
+      it { should contain("NETE_WA_PATH=#{install_dir}/bin").after(/^NETE_WA_ROOT=/) }
+      it { should contain("CAPKIHOME=#{install_dir}/CAPKI").after(/^NETE_WA_PATH=/) }
+      it { should contain("LD_LIBRARY_PATH=#{install_dir}/bin:#{install_dir}/bin/thirdparty:${LD_LIBRARY_PATH}").after(/^CAPKIHOME=/) }
+      it { should contain("PATH=#{install_dir}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/opt/puppetlabs/bin").after(/^LD_LIBRARY_PATH=/) }
+    end
     describe file('/etc/httpd/conf.d/35-ca_sso_web_agent.conf') do
       it { should exist }
       it { should contain 'PassEnv CAPKIHOME' }
@@ -83,6 +97,52 @@ describe 'Declare ca_sso_web_agent class:', unless: UNSUPPORTED_PLATFORMS.includ
     describe service('httpd') do
       it { should be_running }
       it { should be_enabled }
+    end
+  end
+  context 'With local config enabled, web agent enabled, log and trace files enabled' do
+    pp = <<-MANIFEST
+      class { 'ca_sso_web_agent':
+        agent_config_object             => '#{agent_config_object}',
+        enable_local_config             => true,
+        enable_log_file                 => true,
+        enable_trace_file               => true,
+        enable_web_agent                => true,
+        installation_binary             => '#{installation_binary}',
+        installation_zip                => '#{installation_zip}',
+        install_dir                     => '#{install_dir}',
+        install_source                  => '#{install_source}',
+        policy_servers                  => #{policy_servers},
+        register_trusted_host           => #{register_trusted_host},
+        registration_host_config_object => '#{registration_host_config_object}',
+        registration_hostname           => '#{registration_hostname}',
+        registration_password           => '#{registration_password}',
+        registration_policy_server_ip   => '#{registration_policy_server_ip}',
+        registration_username           => '#{registration_username}',
+        version                         => '#{version}',
+      }
+    MANIFEST
+    it 'applies without error' do
+      apply_manifest(pp, catch_failures: true)
+    end
+    describe file("#{install_dir}/config/WebAgent.conf") do
+      it { should exist }
+      it { should contain 'LOCALE=en-US' }
+      it { should contain("HostConfigFile=\"#{install_dir}/config/SmHost.conf\"").before(/^AgentConfigObject=/) }
+      it { should contain('AgentConfigObject="my_proxy_conf"').after(/^HostConfigFile=/) }
+      it { should contain('EnableWebAgent="YES"').after(/^AgentConfigObject=/) }
+      it { should contain('ServerPath="/etc/httpd"').after(/^EnableWebAgent=/) }
+      it { should contain("localconfigfile=\"#{install_dir}/config/LocalConfig.conf\"").after(/^ServerPath=/) }
+      it { should contain("LoadPlugin=\"#{install_dir}/bin/libHttpPlugin.so\"").after(/^localconfigfile/) }
+    end
+    describe file("#{install_dir}/config/LocalConfig.conf") do
+      it { should exist }
+      it { should contain('LogFile="YES"').before(/^LogFileName=/) }
+      it { should contain("LogFileName=\"#{install_dir}/log/WebAgent.log\"").after(/^LogFile="YES"/) }
+      it { should contain('LogFileSize="0"').after(/^LogFileName=/) }
+      it { should contain('TraceFile="YES"').after(/^LogFileSize=/) }
+      it { should contain("TraceFileName=\"#{install_dir}/log/WebAgentTrace.log\"").after(/^TraceFile=/) }
+      it { should contain('TraceFileSize="0"').after(/^TraceFileName=/) }
+      it { should contain("TraceConfigFile=\"#{install_dir}/config/WebAgentTrace.conf\"").after(/^TraceFileSize/) }
     end
   end
 end
