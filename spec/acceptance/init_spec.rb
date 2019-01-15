56bce7f6d399bfe306fd83e1ac91ee9578f09f30
Chris Ewing <caewing@fs.fed.us>
2019-01-14 17:15:39 -0800

    pdk validate - fixed conventions for regex and serverspec.

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -47,56 +47,56 @@ describe 'Declare ca_sso_web_agent class:', unless: UNSUPPORTED_PLATFORMS.includ
     prereq_packages = ['binutils', 'gcc', 'keyutils-libs.i686', 'libidn', 'libidn.i686', 'libstdc++.i686', 'libXext.i686', 'libXrender.i686', 'libXtst.i686', 'ncurses-libs.i686', 'unzip']
     prereq_packages.each do |package|
       describe package(package) do
-        it { should be_installed }
+        it { is_expected.to be_installed }
       end
     end
     describe file('/usr/local/bin/smreghost') do
-      it { should exist }
-      it { should be_symlink }
-      it { should be_linked_to "#{install_dir}/bin/smreghost" }
+      it { is_expected.to exist }
+      it { is_expected.to be_symlink }
+      it { is_expected.to be_linked_to "#{install_dir}/bin/smreghost" }
     end
     describe file("#{install_dir}/config/LocalConfig.conf") do
-      it { should exist }
-      it { should contain('LogFile="NO"').before(/^LogFileName=/) }
-      it { should contain("LogFileName=\"#{install_dir}/log/WebAgent.log\"").after(/^LogFile="NO"/) }
-      it { should contain('LogFileSize="0"').after(/^LogFileName=/) }
-      it { should contain('TraceFile="NO"').after(/^LogFileSize=/) }
-      it { should contain("TraceFileName=\"#{install_dir}/log/WebAgentTrace.log\"").after(/^TraceFile=/) }
-      it { should contain('TraceFileSize="0"').after(/^TraceFileName=/) }
-      it { should contain("TraceConfigFile=\"#{install_dir}/config/WebAgentTrace.conf\"").after(/^TraceFileSize/) }
+      it { is_expected.to exist }
+      it { is_expected.to contain('LogFile="NO"').before(%r{^LogFileName=}) }
+      it { is_expected.to contain("LogFileName=\"#{install_dir}/log/WebAgent.log\"").after(%r{^LogFile="NO"}) }
+      it { is_expected.to contain('LogFileSize="0"').after(%r{^LogFileName=}) }
+      it { is_expected.to contain('TraceFile="NO"').after(%r{^LogFileSize=}) }
+      it { is_expected.to contain("TraceFileName=\"#{install_dir}/log/WebAgentTrace.log\"").after(%r{^TraceFile=}) }
+      it { is_expected.to contain('TraceFileSize="0"').after(%r{^TraceFileName=}) }
+      it { is_expected.to contain("TraceConfigFile=\"#{install_dir}/config/WebAgentTrace.conf\"").after(%r{^TraceFileSize}) }
     end
     describe file("#{install_dir}/config/WebAgent.conf") do
-      it { should exist }
-      it { should contain 'LOCALE=en-US' }
-      it { should contain("HostConfigFile=\"#{install_dir}/config/SmHost.conf\"").before(/^AgentConfigObject=/) }
-      it { should contain('AgentConfigObject="my_proxy_conf"').after(/^HostConfigFile=/) }
-      it { should contain('EnableWebAgent="NO"').after(/^AgentConfigObject=/) }
-      it { should contain('ServerPath="/etc/httpd"').after(/^EnableWebAgent=/) }
-      it { should contain("#localconfigfile=\"#{install_dir}/config/LocalConfig.conf\"").after(/^ServerPath=/) }
-      it { should contain("LoadPlugin=\"#{install_dir}/bin/libHttpPlugin.so\"").after(/^#localconfigfile/) }
+      it { is_expected.to exist }
+      it { is_expected.to contain 'LOCALE=en-US' }
+      it { is_expected.to contain("HostConfigFile=\"#{install_dir}/config/SmHost.conf\"").before(%r{^AgentConfigObject=}) }
+      it { is_expected.to contain('AgentConfigObject="my_proxy_conf"').after(%r{^HostConfigFile=}) }
+      it { is_expected.to contain('EnableWebAgent="NO"').after(%r{^AgentConfigObject=}) }
+      it { is_expected.to contain('ServerPath="/etc/httpd"').after(%r{^EnableWebAgent=}) }
+      it { is_expected.to contain("#localconfigfile=\"#{install_dir}/config/LocalConfig.conf\"").after(%r{^ServerPath=}) }
+      it { is_expected.to contain("LoadPlugin=\"#{install_dir}/bin/libHttpPlugin.so\"").after(%r{^#localconfigfile}) }
     end
     describe file("#{install_dir}/config/WebAgentTrace.conf") do
-      it { should exist }
-      it { should contain('components:  AgentFramework, HTTPAgent, WebAgent').before(/^data:/) }
-      it { should contain('data: Date, Time, Pid, Tid, SrcFile, Function, TransactionID, IPAddr, IPPort, AgentName, Resource, User, Message').after(/^components:/) }
+      it { is_expected.to exist }
+      it { is_expected.to contain('components:  AgentFramework, HTTPAgent, WebAgent').before(%r{^data:}) }
+      it { is_expected.to contain('data: Date, Time, Pid, Tid, SrcFile, Function, TransactionID, IPAddr, IPPort, AgentName, Resource, User, Message').after(%r{^components:}) }
     end
     describe file('/etc/sysconfig/httpd') do
-      it { should exist }
-      it { should contain("NETE_WA_ROOT=#{install_dir}").before(/^NETE_WA_PATH/) }
-      it { should contain("NETE_WA_PATH=#{install_dir}/bin").after(/^NETE_WA_ROOT=/) }
-      it { should contain("CAPKIHOME=#{install_dir}/CAPKI").after(/^NETE_WA_PATH=/) }
-      it { should contain("LD_LIBRARY_PATH=#{install_dir}/bin:#{install_dir}/bin/thirdparty:${LD_LIBRARY_PATH}").after(/^CAPKIHOME=/) }
-      it { should contain("PATH=#{install_dir}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/opt/puppetlabs/bin").after(/^LD_LIBRARY_PATH=/) }
+      it { is_expected.to exist }
+      it { is_expected.to contain("NETE_WA_ROOT=#{install_dir}").before(%r{^NETE_WA_PATH}) }
+      it { is_expected.to contain("NETE_WA_PATH=#{install_dir}/bin").after(%r{^NETE_WA_ROOT=}) }
+      it { is_expected.to contain("CAPKIHOME=#{install_dir}/CAPKI").after(%r{^NETE_WA_PATH=}) }
+      it { is_expected.to contain("LD_LIBRARY_PATH=#{install_dir}/bin:#{install_dir}/bin/thirdparty:${LD_LIBRARY_PATH}").after(%r{^CAPKIHOME=}) }
+      it { is_expected.to contain("PATH=#{install_dir}/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/opt/puppetlabs/bin").after(%r{^LD_LIBRARY_PATH=}) }
     end
     describe file('/etc/httpd/conf.d/35-ca_sso_web_agent.conf') do
-      it { should exist }
-      it { should contain 'PassEnv CAPKIHOME' }
-      it { should contain "LoadModule sm_module #{install_dir}/bin/libmod_sm24.so" }
-      it { should contain "SmInitFile #{install_dir}/config/WebAgent.conf" }
+      it { is_expected.to exist }
+      it { is_expected.to contain 'PassEnv CAPKIHOME' }
+      it { is_expected.to contain "LoadModule sm_module #{install_dir}/bin/libmod_sm24.so" }
+      it { is_expected.to contain "SmInitFile #{install_dir}/config/WebAgent.conf" }
     end
     describe service('httpd') do
-      it { should be_running }
-      it { should be_enabled }
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
     end
   end
   context 'With local config enabled, web agent enabled, log and trace files enabled' do
@@ -125,24 +125,24 @@ describe 'Declare ca_sso_web_agent class:', unless: UNSUPPORTED_PLATFORMS.includ
       apply_manifest(pp, catch_failures: true)
     end
     describe file("#{install_dir}/config/WebAgent.conf") do
-      it { should exist }
-      it { should contain 'LOCALE=en-US' }
-      it { should contain("HostConfigFile=\"#{install_dir}/config/SmHost.conf\"").before(/^AgentConfigObject=/) }
-      it { should contain('AgentConfigObject="my_proxy_conf"').after(/^HostConfigFile=/) }
-      it { should contain('EnableWebAgent="YES"').after(/^AgentConfigObject=/) }
-      it { should contain('ServerPath="/etc/httpd"').after(/^EnableWebAgent=/) }
-      it { should contain("localconfigfile=\"#{install_dir}/config/LocalConfig.conf\"").after(/^ServerPath=/) }
-      it { should contain("LoadPlugin=\"#{install_dir}/bin/libHttpPlugin.so\"").after(/^localconfigfile/) }
+      it { is_expected.to exist }
+      it { is_expected.to contain 'LOCALE=en-US' }
+      it { is_expected.to contain("HostConfigFile=\"#{install_dir}/config/SmHost.conf\"").before(%r{^AgentConfigObject=}) }
+      it { is_expected.to contain('AgentConfigObject="my_proxy_conf"').after(%r{^HostConfigFile=}) }
+      it { is_expected.to contain('EnableWebAgent="YES"').after(%r{^AgentConfigObject=}) }
+      it { is_expected.to contain('ServerPath="/etc/httpd"').after(%r{^EnableWebAgent=}) }
+      it { is_expected.to contain("localconfigfile=\"#{install_dir}/config/LocalConfig.conf\"").after(%r{^ServerPath=}) }
+      it { is_expected.to contain("LoadPlugin=\"#{install_dir}/bin/libHttpPlugin.so\"").after(%r{^localconfigfile}) }
     end
     describe file("#{install_dir}/config/LocalConfig.conf") do
-      it { should exist }
-      it { should contain('LogFile="YES"').before(/^LogFileName=/) }
-      it { should contain("LogFileName=\"#{install_dir}/log/WebAgent.log\"").after(/^LogFile="YES"/) }
-      it { should contain('LogFileSize="0"').after(/^LogFileName=/) }
-      it { should contain('TraceFile="YES"').after(/^LogFileSize=/) }
-      it { should contain("TraceFileName=\"#{install_dir}/log/WebAgentTrace.log\"").after(/^TraceFile=/) }
-      it { should contain('TraceFileSize="0"').after(/^TraceFileName=/) }
-      it { should contain("TraceConfigFile=\"#{install_dir}/config/WebAgentTrace.conf\"").after(/^TraceFileSize/) }
+      it { is_expected.to exist }
+      it { is_expected.to contain('LogFile="YES"').before(%r{^LogFileName=}) }
+      it { is_expected.to contain("LogFileName=\"#{install_dir}/log/WebAgent.log\"").after(%r{^LogFile="YES"}) }
+      it { is_expected.to contain('LogFileSize="0"').after(%r{^LogFileName=}) }
+      it { is_expected.to contain('TraceFile="YES"').after(%r{^LogFileSize=}) }
+      it { is_expected.to contain("TraceFileName=\"#{install_dir}/log/WebAgentTrace.log\"").after(%r{^TraceFile=}) }
+      it { is_expected.to contain('TraceFileSize="0"').after(%r{^TraceFileName=}) }
+      it { is_expected.to contain("TraceConfigFile=\"#{install_dir}/config/WebAgentTrace.conf\"").after(%r{^TraceFileSize}) }
     end
   end
 end
